1f8faa203fed9fe9129cdb8d14bb240d753dc9f6
Oliver Bunyan <Dashb@me.com>
2013-11-20 18:05:21 +0000

    Small Corrections
    changes to the description and readme

--- README.txt
+++ README.txt
@@ -5,4 +5,3 @@ To install this:
 3. bin/pip install git+https://github.com/OliverDashiell/Ludus.git
 4. bin/ludus
 5. browse to http://localhost:8080
-6. to edit you'll need to login - try admin:admin - it usually works
--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ version = '0.04'
 
 setup(name='Ludus',
       version=version,
-      description="Web game maker",
-      long_description="""A web based game maker for 2D games""",
+      description="Web browser game maker.",
+      long_description="""A web based game maker for easy development of 2D games in a multi-user environment.""",
       author='Oliver Dashiell Bunyan',
       author_email='ludus@blueshed.co.uk',
       url='http://www.blueshed.co.uk/ludus',
